15696b674f6518ea6974424f781e860ab6852e64
drewcarter101 <drewcarter101@hotmail.com>
2019-07-13 15:19:49 -0700

    first commit

---
+++ background.js
@@ -0,0 +1,31 @@
+/*
+    This script does not belong to me
+    It was obtained from https://39digits.com/create-a-chrome-extension-to-hide-youtube-comments/
+    Author: Christopher Hamilton
+    All credit goes to him.
+*/
+
+// When the extension is installed or upgraded ...
+chrome.runtime.onInstalled.addListener(function() {  
+    // Replace all rules ...
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      // With a new rule ...
+      chrome.declarativeContent.onPageChanged.addRules([
+        {
+          // That fires when a page's URL contains 'youtube.com'...
+          conditions: [
+            new chrome.declarativeContent.PageStateMatcher({
+              pageUrl: { urlContains: 'youtube.com' },
+            })
+          ],
+          // And shows the extension's page action.
+          actions: [ new chrome.declarativeContent.ShowPageAction() ]
+        }
+      ]);
+    });
+  });
+  
+  
+  chrome.pageAction.onClicked.addListener(function(tab) {  
+    chrome.tabs.executeScript(null, {file: "content.js"});
+  });
